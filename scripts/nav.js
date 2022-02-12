8b1ca5adae077dbff3da20496faef2e985bad08b
Hannah Robinson <hannah.bootcamp@gmail.com>
2022-02-12 15:34:10 +1300

    Make mobile menu close if a menu element is selected

--- scripts/nav.js
+++ scripts/nav.js
@@ -2,6 +2,7 @@ const primaryNav = document.querySelector('.primary-navigation');
 const mobileNavToggle = document.querySelector('.mobile-nav-toggle');
 const toggleIcon = document.querySelector('#toggle-icon');
 
+// Toggle button for mobile menu
 mobileNavToggle.addEventListener('click', () => {
   const visibility = primaryNav.getAttribute('data-visible');
 
@@ -14,4 +15,11 @@ mobileNavToggle.addEventListener('click', () => {
     mobileNavToggle.setAttribute('aria-expanded', false);
     toggleIcon.setAttribute('class', 'fas fa-bars');
   }
+});
+
+// Make mobile menu close if a menu element is selected
+primaryNav.addEventListener('click', () => {
+  primaryNav.setAttribute('data-visible', false);
+  mobileNavToggle.setAttribute('aria-expanded', false);
+  toggleIcon.setAttribute('class', 'fas fa-bars');
 });
